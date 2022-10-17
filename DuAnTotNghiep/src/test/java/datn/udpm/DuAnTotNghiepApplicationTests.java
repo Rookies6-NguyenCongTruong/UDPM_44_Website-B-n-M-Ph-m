229d16acc10714b9e92f19e631ee105fbab1ee8f
duongnvph17448 <duongnvph17448@fpt.edu.vn>
2022-10-17 16:29:41 +0700

    First Commit

---
+++ DuAnTotNghiep/src/test/java/datn/udpm/DuAnTotNghiepApplicationTests.java
@@ -0,0 +1,13 @@
+package datn.udpm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuAnTotNghiepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
